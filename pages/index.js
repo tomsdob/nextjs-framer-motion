3092b72391f7e2788ce451010f2a99dd3c5e2935
Toms D <tomy22783@gmail.com>
2020-05-05 14:57:25 +0300

    changes

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,21 @@
 import fetch from "isomorphic-unfetch";
 
 const Index = (props) => (
-  <div>
-    <div>
+  <div className="w-screen h-screen flex justify-center items-center font-sans bg-gray-100">
+    <div className="-mx-2 flex flex-wrap">
       {props.products.map((product) => (
-        <span key={product.id}>{product.name}</span>
+        <div
+          key={product.id}
+          className="mx-2 p-4 flex flex-col justify-start items-center text-center w-64 h-64 bg-white rounded-lg shadow-md"
+        >
+          <div className="h-32">
+            <img src={product.image} className="max-h-full max-w-full" />
+          </div>
+          <span className="mt-4 text-xl font-medium text-gray-700 leading-tight">
+            {product.name}
+          </span>
+          <span>{product.price}</span>
+        </div>
       ))}
     </div>
   </div>
